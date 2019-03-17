b0dc64041e18ad3353277635df7f16e438d02b6c
shixy96 <shixy@feifanuniv.com>
2019-03-17 23:59:42 +0800

    simple text filter

---
+++ simpleTextFilter/NaiveFilter.py
@@ -0,0 +1,24 @@
+class NaiveFilter():
+
+    '''Filter Messages from keywords
+
+    very simple filter implementation
+
+    >>> f = NaiveFilter()
+    >>> f.add("sexy")
+    >>> f.filter("hello sexy baby")
+    hello **** baby
+    '''
+
+    def __init__(self):
+        self.keywords = set([])
+
+    def parse(self, path):
+        for keyword in open(path):
+            self.keywords.add(keyword.strip().decode('utf-8').lower())
+
+    def filter(self, message, repl="*"):
+        message = unicode(message).lower()
+        for kw in self.keywords:
+            message = message.replace(kw, repl)
+        return message
