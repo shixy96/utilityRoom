6478e6b7b81cb9f3f8a980177c4a3f66ddb5b22f
shixy96 <shixy@feifanuniv.com>
2019-03-26 22:39:56 +0800

    fix web

--- spring/simple-demo/src/main/java/course/web/controller/HelloController.java
+++ spring/simple-demo/src/main/java/course/web/controller/HelloController.java
@@ -14,9 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.hankcs.hanlp.HanLP;
-import com.hankcs.hanlp.seg.common.Term;
-
 import course.web.common.GenericJsonResult;
 import course.web.common.HResult;
 
