d12de894f830cae4b7a26ee160b1f223c8b816bd
shixy96 <shixy@feifanuniv.com>
2019-04-01 12:36:49 +0800

    Update HelloController.java

--- spring/simple-demo/src/main/java/course/web/controller/HelloController.java
+++ spring/simple-demo/src/main/java/course/web/controller/HelloController.java
@@ -4,15 +4,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.seg.common.Term;
 
 import course.web.common.GenericJsonResult;
 import course.web.common.HResult;
@@ -22,7 +25,7 @@ import course.web.common.HResult;
 public class HelloController {
 	@RequestMapping(value = "first")
 	public ModelAndView first(HttpServletRequest request, HttpServletResponse response) {
-		Map<String, Object> map = new HashMap<>();
+		Map<String, Object> map = new HashMap<String, Object>();
 		List<Term> termList = HanLP.segment("今天，刘志军案的关键人物,山西女商人丁书苗在市二中院出庭受审。");
 		map.put("termList", termList);
 		return new ModelAndView("hello", "model", map);
@@ -31,7 +34,7 @@ public class HelloController {
 	@RequestMapping(value = "segment", method = RequestMethod.GET)
 	public @ResponseBody GenericJsonResult<List<Term>> segment(HttpServletRequest request, HttpServletResponse response,
 			@RequestParam(value = "text") String text) {
-		GenericJsonResult<List<Term>> result = new GenericJsonResult<>(HResult.S_OK);
+		GenericJsonResult<List<Term>> result = new GenericJsonResult<List<Term>>(HResult.S_OK);
 		List<Term> textSegment = HanLP.segment(text);
 		result.setData(textSegment);
 		return result;
