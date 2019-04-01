9ca94af3855c183471713eff6ec25f7f1f462d5e
shixy96 <shixy@feifanuniv.com>
2019-04-01 15:30:23 +0800

    first part

--- SensitiveWordAnalysis/SemanticAnalysisPlatform/src/main/java/course/web/controller/HelloController.java
+++ SensitiveWordAnalysis/SemanticAnalysisPlatform/src/main/java/course/web/controller/HelloController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,7 @@ import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.mining.word2vec.WordVectorModel;
 import com.hankcs.hanlp.seg.common.Term;
 
+import course.bll.SensitiveWordManager;
 import course.web.common.GenericJsonResult;
 import course.web.common.HResult;
 
@@ -26,7 +28,10 @@ import course.web.common.HResult;
 @RequestMapping(value = "/hello/")
 public class HelloController {
 	private static final String MODEL_FILE_NAME = "D:/CS224n_NLP/data/test/word2vec.txt";
+	private static final float rate = (float) 0.5;
 	private static WordVectorModel wordVectorModel;
+	@Autowired
+	private SensitiveWordManager sensitiveWordManager;
 	static {
 		try {
 			wordVectorModel = new WordVectorModel(MODEL_FILE_NAME);
@@ -53,7 +58,7 @@ public class HelloController {
 	}
 
 	@RequestMapping(value = "nearest", method = RequestMethod.GET)
-	public @ResponseBody GenericJsonResult<Map<String, Float>> word2vec(HttpServletRequest request,
+	public @ResponseBody GenericJsonResult<Map<String, Float>> nearest(HttpServletRequest request,
 			HttpServletResponse response, @RequestParam(value = "word") String word) throws IOException {
 		GenericJsonResult<Map<String, Float>> result = new GenericJsonResult<Map<String, Float>>(HResult.S_OK);
 		Map<String, Float> wordResult = new HashMap<String, Float>();
@@ -64,4 +69,29 @@ public class HelloController {
 		return result;
 	}
 
+	@RequestMapping(value = "getTextAnalysisResult", method = RequestMethod.GET)
+	public @ResponseBody GenericJsonResult<String> textNearest(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(value = "text") String text) throws IOException {
+		GenericJsonResult<String> result = new GenericJsonResult<String>(HResult.S_OK);
+		String textResult = "";
+		List<Term> textSegment = HanLP.segment(text);
+		for (Term term : textSegment) {
+			boolean sensitiveFlag = sensitiveWordManager.exist(term.word);
+			for (Map.Entry<String, Float> entry : wordVectorModel.nearest(term.word)) {
+				if (entry.getValue() > rate && sensitiveWordManager.exist(entry.getKey())) {
+					sensitiveFlag = true;
+				}
+				if(sensitiveFlag) {
+					break;
+				}
+			}
+			if (sensitiveFlag) {
+				textResult += "*";
+			} else {
+				textResult += term.word;
+			}
+		}
+		result.setData(textResult);
+		return result;
+	}
 }
--- SensitiveWordAnalysis/SemanticAnalysisPlatform/src/main/webapp/WEB-INF/view/hello.jsp
+++ SensitiveWordAnalysis/SemanticAnalysisPlatform/src/main/webapp/WEB-INF/view/hello.jsp
@@ -22,6 +22,7 @@
 			</textarea>
 		</div>
 		<button class='button' onClick="clickBtn()">分词</button>
+		<button class='button' onClick="getTextAnalysisResult()">敏感分析</button>
 		<div class='output areaContent'>
 			<textarea id="outputArea" class="area" rows="10" cols="30"
 				disabled="disabled">
@@ -29,39 +30,17 @@
 		</div>
 	</div>
 
-	<h3 class="title">It's a simple Word2Vec nearest</h3>
-	<div class="operate operate-left">
-		<button class='button' onClick="addWord()">添加词语</button>
-	</div>
-	<div class="operate">
-		<div class='input areaContent'>
-			<textarea id="inputWordArea" class="area" rows="10" cols="30">
-			</textarea>
-		</div>
-		<button class='button' onClick="clickBtnWord2Vec()">nearest</button>
-		<div class='output areaContent'>
-			<textarea id="outputWordArea" class="area" rows="10" cols="30"
-				disabled="disabled">
-			</textarea>
-		</div>
-	</div>
 	<script type="text/javascript">
 		var textIndex = 0;
 		var wordIndex = 0;
 		var placeholders = [ '我新造一个词叫幻想乡你能识别并正确标注词性吗？', '我的希望是希望张晚霞的背影被晚霞映红',
 				'今天，刘志军案的关键人物,山西女商人丁书苗在市二中院出庭受审。', '刘喜杰石国祥会见吴亚琴先进事迹报告团成员' ];
-		var words = [ '中国', '美丽', '购买' ];
 		$(function() {
 		});
 
 		function addText() {
 			textIndex = ++textIndex % placeholders.length;
 			$('#inputArea').val(placeholders[textIndex]);
-		}
-
-		function addWord() {
-			wordIndex = ++wordIndex % words.length;
-			$('#inputWordArea').val(words[wordIndex]);
 		}
 
 		function clickBtn() {
@@ -79,17 +58,14 @@
 			});
 		}
 
-		function clickBtnWord2Vec() {
-			var word = $('#inputWordArea').val();
-			$.get('nearest', {
-				word : word
+		function getTextAnalysisResult() {
+			$("#outputArea").val("");
+			var text = $('#inputArea').val();
+			$.get('getTextAnalysisResult', {
+				text : text
 			}, function(result) {
 				if (result.hr == 0 && result.data) {
-					var segment = '';
-					$.each(result.data, function(index, item) {
-						segment += index + '  ' + item + '\n';
-					});
-					$("#outputWordArea").val(segment);
+					$("#outputArea").val(result.data);
 				}
 			});
 		}
