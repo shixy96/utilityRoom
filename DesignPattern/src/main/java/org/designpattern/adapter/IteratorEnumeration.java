7aa392567cb4dee4d9bbfc1f06fd55f920e5a29a
shixy96 <shixy@feifanuniv.com>
2019-04-28 20:29:12 +0800

    [design pattern] adapter & facade

---
+++ DesignPattern/src/main/java/org/designpattern/adapter/IteratorEnumeration.java
@@ -0,0 +1,23 @@
+package org.designpattern.adapter;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class IteratorEnumeration implements Enumeration {
+	Iterator iterator;
+
+	public IteratorEnumeration(Iterator iterator) {
+		this.iterator = iterator;
+	}
+
+	@Override
+	public boolean hasMoreElements() {
+		return iterator.hasNext();
+	}
+
+	@Override
+	public Object nextElement() {
+		return iterator.next();
+	}
+
+}
